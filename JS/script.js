218b1ed32ca4d02c1acfe1482c0661f6b5228290
Teones <teones.alex.talff99@gmail.com>
2022-02-10 02:49:08 -0300

    layout inicial

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+function entrar() {
+    let telaDeEntrada = document.querySelector(".telaDeEntrada")
+    telaDeEntrada.classList.add("esconder")
+    let telaInicial = document.querySelector(".telaInicial")
+    telaInicial.classList.remove("esconder")
+}
